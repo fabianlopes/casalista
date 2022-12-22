df6d9c4d505a0d0dc43a04281ddcaf43c682b7f8
fabianlopes <60930772+fabianlopes@users.noreply.github.com>
2022-12-22 15:28:21 -0300

    db.js

--- controllers/home.js
+++ controllers/home.js
@@ -15,20 +15,3 @@ module.exports = function (app)
     };
     return HomeController;
 };
-module.exports = function (app) 
-{ var HomeController = {
-            index: function (req, res) {
-                    res.render("home");
-                  },
-            entrar: function (req, res) {
-                    var lista = {
-                        noivo : req.body.noivo,
-                        noiva : req.body.noiva,
-                        presentes : []
-                    };
-                    req.session.lista = lista;
-                    res.redirect("/presentes");
-               }
-    };
-    return HomeController;
-};
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,3 @@ module.exports = function (app) {
     app.get("/", home.index);
     app.post("/entrar", home.entrar);
 };
-module.exports = function (app) {
-    var home = app.controllers.home;
-    app.get("/", home.index);
-    app.post("/entrar", home.entrar);
-};
